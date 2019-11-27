cf3ec493cba74dbfdd689b796aae688855fc7e08
Seán Gravener <sean@gravener.net>
2019-11-27 11:13:07 -0500

    Add bullets

--- main.lua
+++ main.lua
@@ -2,6 +2,13 @@ debug = true
 
 player = { x = 200, y = 710, speed = 150, img = nil }
 
+canShoot = true
+canShootTimerMax = 0.2
+canShootTimer = canShootTimerMax
+
+bulletImg = love.graphics.newImage('assets/bullet_orange0004.png')
+bullets = {}
+
 function love.load(arg)
     player.img = love.graphics.newImage('assets/Aircraft_03.png')
 end
@@ -21,8 +28,31 @@ function love.update(dt)
             player.x = player.x + (player.speed * dt)
         end
     end
+
+    canShoot = canShootTimer - (1 * dt)
+    if canShootTimer < 0 then
+        canShoot = true
+    end
+
+    if love.keyboard.isDown('space', 'rctrl', 'lctrl') and canShoot then
+        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+        table.insert(bullets, newBullet)
+        canShoot = false
+        canShootTimer = canShootTimerMax
+    end
+
+    for i, bullet in ipairs(bullets) do
+        bullet.y = bullet.y - (250 * dt)
+        if bullet.y < 0 then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 function love.draw(dt)
-  love.graphics.draw(player.img, player.x, player.y)
+    love.graphics.draw(player.img, player.x, player.y)
+
+    for i, bullet in ipairs(bullets) do
+        love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    end
 end
