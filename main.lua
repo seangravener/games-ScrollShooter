79a6a886ab5e7a983d23da957603f75149868d7b
Seán Gravener <sean@gravener.net>
2019-11-27 12:38:16 -0500

    Move collision function

--- main.lua
+++ main.lua
@@ -1,13 +1,4 @@
--- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
--- Returns true if two boxes overlap, false if they don't
--- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
--- x2,y2,w2 & h2 are the same, but for the second box
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-    return x1 < x2+w2 and
-           x2 < x1+w1 and
-           y1 < y2+h2 and
-           y2 < y1+h1
-end
+collision = require 'collision'
 debug = true
 
 player = { x = 200, y = 710, speed = 150, img = nil }
@@ -101,14 +92,14 @@ function love.update(dt)
     -- Also, we need to see if the enemies hit our player
     for i, enemy in ipairs(enemies) do
         for j, bullet in ipairs(bullets) do
-            if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight()) then
+            if collision.check(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight()) then
                 table.remove(bullets, j)
                 table.remove(enemies, i)
                 score = score + 1
             end
         end
 
-        if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight())
+        if collision.check(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight())
         and isAlive then
             table.remove(enemies, i)
             isAlive = false
