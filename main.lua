ddf0240a615bece6083ea9234be7c819ddea104b
Seán Gravener <sean@gravener.net>
2019-11-27 09:02:09 -0500

    Add x-axis keboard controls

--- main.lua
+++ main.lua
@@ -7,7 +7,16 @@ function love.load(arg)
 end
 
 function love.update(dt)
+    -- Pressing ESC exits the game
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
 
+    if love.keyboard.isDown('left', 'a') then
+        player.x = player.x - (player.speed * dt)
+    elseif love.keyboard.isDown('right', 'd') then
+        player.x = player.x + (player.speed * dt)
+    end
 end
 
 function love.draw(dt)
