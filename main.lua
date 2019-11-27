2d27cd8327ca8c0f5e8a519211c1f10d2149f2ac
Seán Gravener <sean@gravener.net>
2019-11-27 11:54:01 -0500

    Add collision detection

--- main.lua
+++ main.lua
@@ -1,16 +1,34 @@
+-- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
+-- Returns true if two boxes overlap, false if they don't
+-- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
+-- x2,y2,w2 & h2 are the same, but for the second box
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+    return x1 < x2+w2 and
+           x2 < x1+w1 and
+           y1 < y2+h2 and
+           y2 < y1+h1
+end
 debug = true
 
 player = { x = 200, y = 710, speed = 150, img = nil }
+isAlive = true
+score = 0
 
 canShoot = true
 canShootTimerMax = 0.2
 canShootTimer = canShootTimerMax
-
-bulletImg = love.graphics.newImage('assets/bullet_orange0004.png')
+bulletImg = nil
 bullets = {}
+
+createEnemyTimerMax = 0.4
+createEnemyTimer = createEnemyTimerMax
+enemyImg = nil
+enemies = {}
 
 function love.load(arg)
     player.img = love.graphics.newImage('assets/Aircraft_03.png')
+    bulletImg = love.graphics.newImage('assets/bullet_orange0004.png')
+    enemyImg = love.graphics.newImage('assets/rocket_purple.png')
 end
 
 function love.update(dt)
@@ -47,12 +65,75 @@ function love.update(dt)
             table.remove(bullets, i)
         end
     end
+
+    -- enemy timer
+    createEnemyTimer = createEnemyTimer - (1 * dt)
+    if createEnemyTimer < 0 then
+        createEnemyTimer = createEnemyTimerMax
+
+        -- create an enemy
+        randomNumber = math.random( 10, love.graphics.getWidth() - 10)
+        newEnemy = { x = randomNumber, y = -10, img = enemyImg }
+        table.insert(enemies, newEnemy)
+    end
+
+    for i, enemy in ipairs(enemies) do
+        enemy.y = enemy.y + (200 * dt)
+        if enemy.y > 850 then
+            table.remove(enemies, i)
+        end
+    end
+
+    -- run our collision detection
+    -- Since there will be fewer enemies on screen than bullets we'll loop them first
+    -- Also, we need to see if the enemies hit our player
+    for i, enemy in ipairs(enemies) do
+        for j, bullet in ipairs(bullets) do
+            if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight()) then
+                table.remove(bullets, j)
+                table.remove(enemies, i)
+                score = score + 1
+            end
+        end
+
+        if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight())
+        and isAlive then
+            table.remove(enemies, i)
+            isAlive = false
+        end
+    end
+
+    if not isAlive and love.keyboard.isDown('r') then
+        -- remove all our bullets and enemies from screen
+        bullets = {}
+        enemies = {}
+
+        -- reset timers
+        canShootTimer = canShootTimerMax
+        createEnemyTimer = createEnemyTimerMax
+
+        -- move player back to default position
+        player.x = 50
+        player.y = 710
+
+        -- reset our game state
+        score = 0
+        isAlive = true
+    end
 end
 
 function love.draw(dt)
-    love.graphics.draw(player.img, player.x, player.y)
+    if isAlive then
+        love.graphics.draw(player.img, player.x, player.y)
+    else
+        love.graphics.print("Press 'R' to restart", love.graphics:getWidth()/2-50, love.graphics:getHeight()/2-10)
+    end
 
     for i, bullet in ipairs(bullets) do
         love.graphics.draw(bullet.img, bullet.x, bullet.y)
+    end
+
+    for i, enemy in ipairs(enemies) do
+        love.graphics.draw(enemy.img, enemy.x, enemy.y)
     end
 end
